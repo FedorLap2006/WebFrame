3079723b726649249322d5f16b13e84ca2d63cf8
Fedor Lapshin <42876964+FedorLap2006@users.noreply.github.com>
2019-01-30 00:11:53 +0300

    Optional Update

--- WebFrame.go
+++ WebFrame.go
@@ -16,8 +16,8 @@ type Context struct {
 	RemoteAddr string
 }
 
-func (this* Context) GetPage(filename string,ldelim string,rdelim string) (*template.Template,error){
-	tmp,err := template.New("").Delims(ldelim,rdelim).ParseFiles(filename)
+func (this* Context) GetPage(filename... string,ldelim string,rdelim string,exec bool) (*template.Template,error){
+	tmp,err := template.New("").Delims(ldelim,rdelim).ParseFiles(filename...)
 	return tmp,err
 }
 
